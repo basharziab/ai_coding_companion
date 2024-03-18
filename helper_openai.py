5388e3c2b37d70444b3f40a73e4d0644e5448814
waqas <waqasahsan84@gmail.com>
2024-03-19 01:45:31 +0500

    Update helper_openai.py

--- helper_openai.py
+++ helper_openai.py
@@ -9,7 +9,7 @@ def openai_text(prompt, entered_api_key):
             {"role": "system", "content": "You are a helpful programmer. You only respond if the prompt is relevent to programming. You only communicate using markdown with proper formatting and headings of each task."},
             {"role": "user", "content": prompt}
         ],
-        max_tokens=300,  # the maximum number of tokens to generate in the completion
+        # max_tokens=300,  # the maximum number of tokens to generate in the completion
         temperature=0,  # this is the degree of randomness of the model's output
     )
     # print(f"RESPONSE OPENAI : {response}")
